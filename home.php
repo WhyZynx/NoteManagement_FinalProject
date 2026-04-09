5c35f7cdbf4bd61d196af9c57edb0a3972b143ed
Vy <vongocthanhvy289@gmail.com>
2026-04-09 11:53:06 +0700

    Chua xong home

---
+++ home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NoteFlow</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="custom-navbar navbar navbar-expand-lg">
+        <div class="logo">NotesFlow</div>
+
+        <div class="d-flex align-items-center gap-3 nav-right">
+            <div class="d-flex align-items-center gap-1 lang">
+                <i class="fa-solid fa-globe"></i>
+                <span>EN</span>
+            </div>
+            <a href="login.php" class="btn signUp-btn">Login</a>
+            <a href="register.php" class="btn signUp-btn">Sign Up</a>
+        </div>
+
+    </nav>
+
+    <header class="hero-section">
+        <div class="hero-content">
+            <div class="text-group">
+                <h1 class="hero-title">Free your mind</h1>
+                <p class="hero-subtitle">
+                    Move your thoughts from your head to a safe space, exactly the way you imagined them.
+                </p>
+            </div>
+            <a href="login.php" class="btn-start">Let's start</a>
+        </div>
+        
+        <div class="hero-image">
+            <img src="web_img/duck.png" alt="NotesFlow Illustration">
+        </div>
+    </header>
+
+    <section class="features-intro">
+        <div class="container">
+            <div class="section-header">
+                <h2>Make the most of your ideas and your time</h2>
+                <p>Capture everything that is, was, or could be important and access it anytime, anywhere.</p>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+</html>
